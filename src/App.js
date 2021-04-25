b6a4e850b4a1961c6c24c6ef430cc54d8ada61f6
github_anaeem59 <anaeem59@gmail.com>
2021-04-25 16:01:39 +0530

    Day Three

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import LoginScreen from './LoginScreen'
 import { auth } from './firebase';
 import { useDispatch, useSelector } from 'react-redux';
 import { login, logout, selectUser } from './features/userSlice';
+import ProfileScreen from './ProfileScreen'
 
 function App() {
   const user = useSelector(selectUser);
@@ -20,13 +21,12 @@ function App() {
         })
         )
       } else {
-        dispatch(logout);
+        dispatch(logout());
       }
-      console.log(userAuth);
     })
 
     return unsubscribe;
-  }, [])
+  }, [dispatch])
 
   return (
     <div className="app"> 
@@ -35,6 +35,9 @@ function App() {
          <LoginScreen />
        ) : (
         <Switch>
+          <Route path="/profile">
+            <ProfileScreen />
+          </Route>
          <Route exact path="/" >
           <HomeScreen />
          </Route>
